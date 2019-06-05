a8f474e6a35b9007754cc00499a7f3e75782f2ec
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-06-05 00:32:00 -0500

    How to Change Footer Copyright Text in Magento2

--- app/code/Essentials/ClassPreference/Block/Html/Footer.php
+++ app/code/Essentials/ClassPreference/Block/Html/Footer.php
@@ -3,47 +3,22 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Essentials\ClassPreference\Block\Html;
 
 /**
  * Html page footer block
  */
-class Footer
+class Footer extends \Magento\Theme\Block\Html\Footer
 {
     /**
-     * Copyright information
-     *
-     * @var string
-     */
-    private $copyright;
-
-    /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    private $scopeConfig;
-
-    /**
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     */
-    public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-    ) {
-        $this->scopeConfig = $scopeConfig;
-    }
-
-    /**
-     * Retrieve copyright information
-     *
-     * @return string
+     * @inheritdoc
      */
     public function getCopyright(): string
     {
-        if (!$this->copyright) {
-            $this->copyright = $this->scopeConfig->getValue(
-                'design/footer/copyright',
-                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-            );
-        }
-        return __($this->copyright);
+        $copyright = parent::getCopyright();
+
+        return '| ' . $copyright . ' |';
     }
 }

a563898471c2d165b6c7cb7ba45ef37b321e8919
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-06-07 00:06:56 -0500

    Log full frontend action name with plugin after request dispatched
    - Add strict type declaration;

--- app/code/Essentials/LoggingPlugin/Plugin/App/Action/ActionNameLogger.php
+++ app/code/Essentials/LoggingPlugin/Plugin/App/Action/ActionNameLogger.php
@@ -4,6 +4,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Essentials\LoggingPlugin\Plugin\App\Action;
 
 use Magento\Framework\App\ResponseInterface;
